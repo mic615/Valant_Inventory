05d10728341579f62eace0cec8522a6414c98aae
mic615 <mic615@gmail.com>
2018-10-21 12:43:02 -0500

    Initial commit with basic CRUD opertations .

---
+++ Inventory/Controllers/ItemsController.cs
@@ -0,0 +1,53 @@
+﻿using Inventory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Inventory.Controllers
+{
+    public class ItemsController : ApiController
+    {
+        public List<Item> items = new List<Item>();
+
+        public ItemsController()
+        {
+            items.Add(new Item {  IsExpired = false, Type = "Eggs",Label = 1});
+            items.Add(new Item {  IsExpired = false, Type = "Milk", Label = 2 });
+            items.Add(new Item {  IsExpired = false, Type = "Bannana", Label = 3});
+        }
+
+        // GET: api/Items
+        public List<Item> Get()
+        {
+            return items;
+        }
+
+        // GET: api/Items/5
+        public Item Get(int label)
+        {
+            Item item = items.Where(x => x.Label == label).FirstOrDefault();
+            return item;
+        }
+
+        // POST: api/Items
+        public void Post(Item item)
+        {
+            items.Add(item);
+        }
+
+        // PUT: api/Items/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Items/5
+        public void Delete(int label)
+        {
+            Item item = items.Where(x => x.Label == label).FirstOrDefault();
+            items.Remove(item);
+        }
+    }
+}
---
+++ Inventory/Models/Item.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Inventory.Models
+{
+    public class Item
+    {
+        public int Label { get; set; } = 0;
+        public string Type { get; set; } = "";
+        public System.DateTime Expiration { get; set; }
+        public bool IsExpired { get; set; } = false;
+    }
+}
